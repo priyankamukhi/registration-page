c4976737d99fe165556d3ec5670ee6d962e1fe2e
Priyanka_Mukhi <priyankamukhi96@gmail.com>
2021-12-23 12:45:56 +0530

    registration page

---
+++ registration/Models/regs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace registration.Models
+{
+    public class regs
+    {
+        public int regsid { get; set; }
+        public string regsname { get; set; }
+        public Int32 regsrollno { get; set; }
+        public int regsage { get; set; }
+        public string regsemailid { get; set; }
+        public Int64 regscontactno { get; set; }
+    }
+}
